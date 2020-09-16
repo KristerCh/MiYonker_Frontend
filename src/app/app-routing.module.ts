40dbbcbad2d2b0f23882de9a4137d38434ba80a1
Javier Argueta <javychavez10@gmail.com>
2020-09-15 23:12:09 -0600

    Creación de formulario para registrar las partes de un vehículo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { ListPartsComponent } from './components/list-parts/list-parts.component
 import { AboutComponent } from './components/about/about.component';
 import { ProductComponent } from './components/product/product.component';
 import { Routes, RouterModule } from '@angular/router';
+import { PartsVehicleFormComponent} from './components/parts-vehicle-form/parts-vehicle-form.component';
 
 const routes: Routes = [
   {path: '', component: AboutComponent},
   {path: 'list-parts', component: ListPartsComponent },
-  {path: 'product', component: ProductComponent}
+  {path: 'product', component: ProductComponent},
+  {path: 'partsVehicle-form', component: PartsVehicleFormComponent}
 ];
 
 @NgModule({
