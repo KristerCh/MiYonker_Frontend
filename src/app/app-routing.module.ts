493420a336c6e7f988efe7d050967a6481b74a77
wendyny <wenyen9306@gmail.com>
2020-09-15 22:06:24 -0600

    "formulariosStores"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { ListPartsComponent } from './components/list-parts/list-parts.component
 import { AboutComponent } from './components/about/about.component';
 import { ProductComponent } from './components/product/product.component';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreFormComponent } from './components/store-form/store-form.component';
 
 const routes: Routes = [
   {path: '', component: AboutComponent},
   {path: 'list-parts', component: ListPartsComponent },
-  {path: 'product', component: ProductComponent}
+  {path: 'product', component: ProductComponent},
+  {path: 'store-form', component: StoreFormComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/store-form/store-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-form',
+  templateUrl: './store-form.component.html',
+  styleUrls: ['./store-form.component.css']
+})
+export class StoreFormComponent {
+
+  constructor() { }
+
+}
