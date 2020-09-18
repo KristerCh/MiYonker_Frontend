74a8ff335795b3ccff05fb8c47bdbacdd56f26a8
Javier Argueta <javychavez10@gmail.com>
2020-09-17 22:44:21 -0600

    Creación de form parts vehicle

---
+++ src/app/components/parts-vehicle-form/parts-vehicle-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parts-vehicle-form',
+  templateUrl: './parts-vehicle-form.component.html',
+  styleUrls: ['./parts-vehicle-form.component.css']
+})
+export class PartsVehicleFormComponent {
+
+  constructor() { }
+
+
+}
